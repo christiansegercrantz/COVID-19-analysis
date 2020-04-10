9b4b6636db802234ac063064021c3c5a8751ebf5
Christian Segercrantz <segercc1@aalto.fi>
2020-04-10 11:42:30 +0300

    Added new cases bar plot

--- Main.R
+++ Main.R
@@ -1,8 +1,10 @@
 rm(list = ls())
 
-#install.packages("readxl")
+#install.packages("readxl","dplyr")
 library("readxl")
 library("dplyr")
+library("ggplot2")
+
 data <- data.frame(read_excel("data.xlsx"))
 data <- data[order(data$countriesAndTerritories, data$dateRep),]
 data <- data %>%
@@ -12,3 +14,11 @@ data <- data %>%
   group_by(countriesAndTerritories) %>%
   mutate("cumulative deaths" = cumsum(deaths))
 
+country = "Finland"
+
+### Plot the new cases of the country
+new_cases_per_country<-ggplot(data=data[data$countriesAndTerritories == country,], aes(x=dateRep, y=cases)) +
+  geom_bar(stat="identity") +
+  geom_text(aes(label=cases))
+new_cases_per_country
+###
